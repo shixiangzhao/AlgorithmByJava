edab21bc98ed01f7da0333512c88b6c1dcab2027
Shixiang Zhao <zsxfordream@163.com>
2018-03-01 18:01:15 +0800

    update initial order test

--- src/main/java/shixzh/abj/basetype/InitialOrderTest.java
+++ src/main/java/shixzh/abj/basetype/InitialOrderTest.java
@@ -34,7 +34,7 @@ public class InitialOrderTest extends InitialOrderParent {
     }
 }
 
-class InitialOrderParent {
+class InitialOrderParent extends InitialOrderParent2{
 
     // 静态变量 
     public static String staticField = "父类-静态变量";
@@ -56,5 +56,30 @@ class InitialOrderParent {
     // 构造器 
     public InitialOrderParent() {
         System.out.println("父类-构造器");
+    }
+}
+
+class InitialOrderParent2 {
+
+    // 静态变量 
+    public static String staticField = "父类2-静态变量";
+    // 变量 
+    public String field = "父类2-变量";
+
+    // 静态初始化块 
+    static {
+        System.out.println(staticField);
+        System.out.println("父类2-静态初始化块");
+    }
+
+    // 初始化块 
+    {
+        System.out.println(field);
+        System.out.println("父类2-初始化块");
+    }
+
+    // 构造器 
+    public InitialOrderParent2() {
+        System.out.println("父类2-构造器");
     }
 }
